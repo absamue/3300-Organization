e2d51b30dc559b37e8997807feabf8966ef2137c
Andrew Samuels <absamue@g.clemson.edu>
2017-03-04 14:38:14 -0500

    starting printing

--- simulator.py
+++ simulator.py
@@ -24,6 +24,7 @@ OR_ADDR = 17
 cs = [[0 for x in range(32)] for y in range(22)]
 mem = [0 for x in range(512)]
 CSIR = [0 for x in range(22)]
+CSIGNALS = [0 for x in range(22)]
 PC = 0 
 MAR = 0
 IR = 0
@@ -32,6 +33,7 @@ ACC = 0
 TMP = 0
 CSAR = 0
 BUS = 0
+CYCLE = 1
 halt = False
 
 #generate cs
@@ -80,6 +82,23 @@ cs[13][NEXT] = 4
 cs[14][NEXT] = 0
 cs[15][NEXT] = 0
 
+CSIGNALS[0] = "MAR_in PC_out"
+CSIGNALS[1] = "PC_in IR_out"
+CSIGNALS[2] = "pc_incr read"
+CSIGNALS[3] = "IR_in MDR_out"
+CSIGNALS[4] = "br_table"
+CSIGNALS[5] = "MAR_in IR_out"
+CSIGNALS[6] = "read"
+CSIGNALS[7] = "ACC_in MDR_out"
+CSIGNALS[8] = "MAR_in IR_out"
+CSIGNALS[9] = "read"
+CSIGNALS[10] = "ACC_out alu_add"
+CSIGNALS[11] = "ACC_in TMP_out"
+CSIGNALS[12] = "MAR_in IR_out"
+CSIGNALS[13] = "MDR_in ACC_out"
+CSIGNALS[14] = "write"
+CSIGNALS[15] = "or_addr"
+
 #hex to int
 def hextoint(val):
     ret = int(val, 16)
@@ -106,7 +125,7 @@ def mem_print():
         if x==9:
             print("")
             print("           ", end='')
-    print("")
+    print("\n")
 
 #process mem
 def process():
@@ -134,31 +153,36 @@ def execute(opcode, addr):
 #        6 : jmpi,
         7 : halt,
     }
-    print(opcode, addr)
     options[opcode](addr)
 
 def load(addr):
     global PC, IR, MAR, MDR, ACC 
     #T1
+    cycle_print(addr)
     MAR = int(PC)
     #T2
+    cycle_print(addr)
     MDR = mem[MAR]
     PC += 1
     #T3
+    cycle_print(addr)
     IR = MDR
     #T4br table
     #T5 
+    cycle_print(addr)
     MAR = addr
     #T6
+    cycle_print(addr)
     MDR = mem[MAR]
     #T7
+    cycle_print(addr)
     ACC = hextoint(MDR)
-    print("load success")
 
 def add(addr):
     global PC, IR, MAR, MDR, ACC 
     #T1
     MAR = int(PC)
+    cycle_print(addr)
     #T2
     MDR = mem[MAR]
     PC += 1
@@ -173,7 +197,6 @@ def add(addr):
     TMP = hextoint(MDR) + ACC
     #T8
     ACC = TMP
-    print("add success")
 
 def store(addr):
     global PC, IR, MAR, MDR, ACC 
@@ -191,7 +214,6 @@ def store(addr):
     MDR = ACC
     #T7
     mem[MAR] = MDR
-    print("store success")
 
 def brz(addr):
     global PC, IR, MAR, MDR, ACC 
@@ -205,7 +227,6 @@ def brz(addr):
     #T4 br table
     if ACC == 0:
         PC = addr
-    print("brz success")
 
 def halt(addr):
     global PC, IR, MAR, MDR, ACC, halt
@@ -219,9 +240,21 @@ def halt(addr):
     #T4 br table
     halt = True
 
-        
+def cycle_print(addr):
+    global PC, IR, MAR, MDR, ACC, halt, CYCLE
+
+    print ("%4s:%3s %3s %3s %3s %3s %3s %5s  " 
+           % (CYCLE, PC, IR, MAR, MDR, ACC, TMP, CSAR), end='')
+    CYCLE += 1
+    for i in range(16):
+        print (cs[addr][i], end='')
+    print ("|%2s|%1s  " % (format(cs[addr][NEXT], '02x'), cs[addr][OR_ADDR])
+           , CSIGNALS[addr])
+
 #main
 read()
 mem_print()
+print("cycle  PC  IR MAR MDR ACC TMP  CSAR          CSIR            cntl signals")
+print("    +---+---+---+---+---+---+/----//---------------------//---------------/")
 process()
 mem_print()
